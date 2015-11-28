9266dd95c4693d2f9267c9d32928910d7a2678d4
Brien Crean <brien.crean@gmail.com>
2015-11-28 14:04:08 -0800

    added update, destroy actions

--- app/controllers/drill_groups_controller.rb
+++ app/controllers/drill_groups_controller.rb
@@ -8,9 +8,11 @@ class DrillGroupsController < ApplicationController
 
   def create
     # drill_group_params method defined below
+    # category = params[:category_id]
+
     drill_group_params
     @drill_group = DrillGroup.new(drill_group_params)
-
+    # @drill_group.category = category
 
     if @drill_group.save
       # need to test once category CRUD implemented
@@ -23,34 +25,40 @@ class DrillGroupsController < ApplicationController
 
   def show
     # note before_action has find_drill_group
-
-    respond_to do |format|
-    format.html {render}
-    # if we end up using AJAX
-    format.json {render json: @q.to_json}
-end
-
+    # need to also find category
   end
+
 
   def edit
     # note before_action has find_drill_group
+
   end
 
   def update
     # note before_action has find_drill_group
+    drill_group_params
+
+    if @drill_group.update(drill_group_params)
+      redirect_to category_snippet_path(@category, @snippet), notice: "Drill Group updated!"
+    else
+      render :edit
+    end
   end
 
   def destroy
     # note before_action has find_drill_group
+    @drill_group.destroy
+
+    redirect_to root_path, alert: "Drill Group deleted!"
   end
 
 
   def drill_group_params
-        params.require(:drill_group).permit([:name, :description])
+    params.require(:drill_group).permit([:name, :description])
   end
 
   def find_drill_group
-      @drill_group = DrillGroup.find(params[:id])
+    @drill_group = DrillGroup.find(params[:id])
   end
 
 end
--- app/models/drill_group.rb
+++ app/models/drill_group.rb
@@ -1,3 +1,6 @@
 class DrillGroup < ActiveRecord::Base
   belongs_to :category
+
+  validates :name, presence: true
+  validates :description, presence: true
 end
