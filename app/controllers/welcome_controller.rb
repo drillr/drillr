8269e38f7a8b422f363b13187c60176bcb5d1fb9
MonikaP <piechatzek@hotmail.com>
2015-11-29 14:31:36 -0800

    add splash page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,21 @@
 class WelcomeController < ApplicationController
+  helper_method :resource_name, :resource, :devise_mapping
 
 
   def index
-
+    render layout: 'splash'
   end
 
 
+  def resource_name
+    :user
+  end
 
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
