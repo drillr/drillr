a3d4ddb7655ba546b2526bf1c34e0ce7cd2f88ec
MonikaP <piechatzek@hotmail.com>
2015-11-29 19:24:00 -0800

    redirect new user to login and flash

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,13 @@
 class WelcomeController < ApplicationController
   helper_method :resource_name, :resource, :devise_mapping
 
-
   def index
-    render layout: 'splash'
+    if user_signed_in?
+      redirect_to dashboard_path
+    else
+      render layout: 'splash'
+    end
   end
-
 
   def resource_name
     :user
