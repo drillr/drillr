b864a51e8bedf1c2b714ce10f2974c6b76c1631f
Alex Taylor <alexmctaylorpants@gmail.com>
2015-11-29 18:26:32 -0800

    Minor fixes to drill show page

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -23,8 +23,19 @@ class AnswersController < ApplicationController
           Rails.logger.info "Used hint, so user will earn #{earned_points} instead of #{original_points}"
         end
 
-
         @user.points = @user.points + earned_points
+
+        # add to training plan
+        selected_drill = UserDrill.where(user: @user, drill: @drill)
+        if !selected_drill.present?
+          Rails.logger.info "The user has not added this drill yet, so adding it for them and marking it complete..."
+          selected_drill = UserDrill.new
+          selected_drill.user = @user
+          selected_drill.drill = @drill
+        end
+
+        selected_drill.completed = true
+        selected_drill.save
 
         achievements = Achievement.all
 
